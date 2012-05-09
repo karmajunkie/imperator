5a8471913e79e7829a66cc3f2482a2ad454504e1
Nick Maloney <ngmaloney@gmail.com>
2012-05-09 13:17:28 -0400

    Removed pry dependency

--- lib/imperator.rb
+++ lib/imperator.rb
@@ -2,7 +2,6 @@ require 'active_attr'
 require "imperator/version"
 require 'imperator/invalid_command_error'
 require 'imperator/command'
-require 'pry'
 
 module Imperator
   # Your code goes here...

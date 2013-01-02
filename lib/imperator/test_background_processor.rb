bb90619e8c0c3f1dfc5e236c5009203923fceacd
karmajunkie <keith.gaddis@gmail.com>
2013-01-02 12:27:29 -0600

    Add a class option to TestBackgroundProcessor to commit immediately
    defaults to the old behavior (delay and add to commits) but
    allows test to force commands to process immediately

--- lib/imperator/test_background_processor.rb
+++ lib/imperator/test_background_processor.rb
@@ -2,10 +2,11 @@ class Imperator::TestBackgroundProcessor
   @commits = []
   class << self
     attr_accessor :commits
+    attr_accessor :commit_mode
   end
 
   def self.commit(command, options = nil) 
     @commits << command
-    command.perform!
+    command.perform! if self.commit_mode == :immediate
   end
 end
